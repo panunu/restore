b64287aa6994b67abcbf09855eea93d117f37079
Panu Leppäniemi <me@panuleppaniemi.com>
2012-01-09 12:16:42 +0200

    Added inheritable AbstractEntity.

---
+++ src/Shop/FrameworkBundle/Entity/AbstractEntity.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Shop\FrameworkBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM,
+    \ReflectionClass,
+    \ReflectionException;
+
+abstract class AbstractEntity
+{   
+    /**
+     * Reflects get and set methods.
+     * 
+     * @param  string $method
+     * @param  mixed  $parameter
+     * @return mixed
+     */
+    public function __call($method, $parameter = null)
+    {
+        $propertyName = lcfirst(substr($method, 3));
+        $accessor     = substr($method, 0, 3);
+        
+        if (!in_array($accessor, array('get', 'set'))) {
+            $propertyName = $method;
+            $accessor     = null;
+        }
+                        
+        $class = new ReflectionClass($this);
+        $class = !strstr($class->getName(), 'Proxy') ?
+            $class : $class->getParentClass();
+                
+        if (!$class->hasProperty($propertyName) || $class->getProperty($propertyName)->isPrivate())
+        {
+            throw new ReflectionException(
+                "No such property as {$propertyName} in {$class->getName()}"
+            );
+        }
+        
+        $property = $class->getProperty($propertyName);
+        $property->setAccessible(true);
+        
+        if (!$accessor || $accessor == 'get') {
+            return $property->getValue($this);
+        }
+                
+        $property->setValue($this, $parameter);
+        
+        return $this;
+    }
+}
