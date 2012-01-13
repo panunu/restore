0524de25e4212e8d541080009567447cda935986
Panu Leppäniemi <me@panuleppaniemi.com>
2012-01-13 19:13:38 +0200

    Added Cart persisting to Session.

--- src/Shop/CartBundle/Service/CartService.php
+++ src/Shop/CartBundle/Service/CartService.php
@@ -2,7 +2,9 @@
 
 namespace Shop\CartBundle\Service;
 
-use \Symfony\Component\HttpFoundation\Session;
+use \Symfony\Component\HttpFoundation\Session,
+    Shop\CartBundle\Model\Cart,
+    Shop\ProductBundle\Entity\Product;
 
 class CartService
 {
@@ -11,8 +13,71 @@ class CartService
      */
     protected $session;
     
+    /**
+     * @var Cart
+     */
+    protected $cart;
+    
     public function __construct(Session $session)
     {
         $this->session = $session;
+    }
+    
+    /**
+     * @param  Product $product
+     * @return CartService 
+     */
+    public function addProductToCart(Product $product)
+    {
+        $this->getCart()->addProduct($product);
+                
+        return $this->flush();
+    }
+    
+    /**
+     * @param  Product $product
+     * @return CartService 
+     */
+    public function removeProductFromCart(Product $product)
+    {
+        $this->getCart()->removeProduct($product);
+                
+        return $this->flush();
+    }
+    
+    /**
+     * @return int
+     */
+    public function countProductsInCart()
+    {
+        return count($this->getCart()->getProducts());
+    }
+    
+    /**
+     * @return Cart|null
+     */
+    public function getCart()
+    {
+        return $this->cart = $this->session->get('cart') ?: new Cart();
+    }
+    
+    /**
+     * @return boolean
+     */
+    public function hasCart()
+    {
+        return $this->session->get('cart') != null;
+    }
+    
+    /**
+     * Flushes the session, persisting possible changes to Cart.
+     * 
+     * @return CartService
+     */
+    protected function flush()
+    {
+        $this->session->set('cart', $this->cart);
+        
+        return $this;
     }
 }
--- src/Shop/CartBundle/Tests/Service/CartServiceTest.php
+++ src/Shop/CartBundle/Tests/Service/CartServiceTest.php
@@ -19,5 +19,64 @@ class CartServiceTest extends TestCase
         parent::setUp();
         
         $this->service = new CartService($this->getContainer()->get('session'));
+        $this->product = $this->getFixtureFactory()->get('ProductBundle\Entity\Product');
+    }
+        
+    /**
+     * @test
+     * @group service
+     * @group cart
+     */
+    public function knowsIfDoesNotHaveCartStoredInSession()
+    {
+        $this->assertFalse($this->service->hasCart());
+    }
+    
+    /**
+     * @test
+     * @group service
+     * @group cart
+     */
+    public function addsProductToCart()
+    {
+        $this->service->addProductToCart($this->product);
+        
+        $this->assertEquals(
+            array($this->product),
+            $this->service->getCart()->getProducts()
+        );
+    }
+    
+    /**
+     * @test
+     * @group service
+     * @group cart
+     */
+    public function removesProductToCart()
+    {
+        $this->service->addProductToCart($this->product);
+        $this->service->addProductToCart($this->product);
+        $this->service->removeProductFromCart($this->product);
+        
+        $this->assertEquals(
+            1,
+            count($this->service->getCart()->getProducts())
+        );
+    }
+    
+    /**
+     * @test
+     * @group service
+     * @group cart
+     */
+    public function countsProductsInCart()
+    {
+        $this->service->addProductToCart($this->product);
+        $this->service->addProductToCart($this->product);
+        
+        $this->assertEquals(
+            2,
+            $this->service->countProductsInCart()
+        );
     }
 }
