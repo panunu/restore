534bdd8264188649671a6a0e2eff5ccea59fb457
Panu Leppäniemi <me@panuleppaniemi.com>
2012-01-14 11:57:32 +0200

    Updated FixtureFactory.

--- src/Shop/FrameworkBundle/Test/Fixtures/FixtureFactory.php
+++ src/Shop/FrameworkBundle/Test/Fixtures/FixtureFactory.php
@@ -4,7 +4,9 @@ namespace Shop\FrameworkBundle\Test\Fixtures;
 
 use \Doctrine\ORM\EntityManager,
     Xi\Doctrine\Fixtures\FixtureFactory as BaseFixtureFactory,
-    Xi\Doctrine\Fixtures\FieldDef;
+    Xi\Doctrine\Fixtures\FieldDef,
+    \Shop\ProductBundle\Entity\Tax,
+    \DateTime;
 
 class FixtureFactory extends BaseFixtureFactory
 {
@@ -14,6 +16,7 @@ class FixtureFactory extends BaseFixtureFactory
         
         $this->setEntityNamespace('Shop');
         $this->setUpFixtures();
+        $this->persistOnGet();
     }
     
     /**
@@ -21,9 +24,16 @@ class FixtureFactory extends BaseFixtureFactory
      */
     protected function setUpFixtures()
     {
+        $this->defineEntity('ProductBundle\Entity\Tax', array(
+            'type'     => Tax::TYPE_GENERAL,
+            'percent'  => 23.00,
+            'validity' => new DateTime('2000-01-01')
+        ));
+        
         $this->defineEntity('ProductBundle\Entity\Product', array(
             'name'  => FieldDef::sequence("Product %d"),
             'price' => 5.00,
+            'tax'   => Tax::TYPE_GENERAL
         ));
         
         return $this;
