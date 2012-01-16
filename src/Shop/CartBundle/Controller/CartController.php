22426cc4ba40f459b2a0961bb2b78346babf81c4
Panu Leppäniemi <me@panuleppaniemi.com>
2012-01-16 22:56:40 +0200

    Initial Cart pages.

--- src/Shop/CartBundle/Controller/CartController.php
+++ src/Shop/CartBundle/Controller/CartController.php
@@ -9,8 +9,17 @@ class CartController extends Controller
     public function viewAction()
     {
         return $this->render('ShopCartBundle:Cart:view.html.twig', array(
-            'cart' => $this->getCartService()->getCart()
+            'cart' => $this->getCartService()->getCart() // TODO: Transfer into Model that contains totals etc.
         ));
+    }
+    
+    public function addAction($product)
+    {
+        $product = $this->getProductService()->getProductById($product);
+        
+        $this->getCartService()->addProductToCart($product);
+        
+        return new \Symfony\Component\HttpFoundation\Response('ok'); // TODO: Refactor, maybe JSON response?
     }
     
     /**
@@ -19,5 +28,13 @@ class CartController extends Controller
     protected function getCartService()
     {
         return $this->get('shop_cart.service.cart');
+    }
+    
+    /**
+     * @return CartService
+     */
+    protected function getProductService()
+    {
+        return $this->get('shop_product.service.product');
     }
 }
